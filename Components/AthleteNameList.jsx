542d76d219d7863444f3f0c35c55b6d336070da8
Paxton Caldwell <caldwellp20@gmail.com>
2024-04-17 23:05:24 -0400

    passes inputValues to TimerScreen

--- Components/AthleteNameList.jsx
+++ Components/AthleteNameList.jsx
@@ -6,27 +6,36 @@ const AthleteNameList = ({ navigation }) => {
   const route = useRoute();
   const userNumber = route.params?.userNumber || '';
 
-  const [data, setData] = useState(Array.from({ length: userNumber }, (_, index) => ({ key: String(index+1) })));
-  const [textInputs, setTextInputs] = useState(Array.from({ length: userNumber }, () => ''));
+  const [inputValues, setInputValues] = useState(Array.from({ length: userNumber }, () => ''));
 
-  const renderItem = ({ item }) => (
+  const renderItem = ({ item, index }) => (
     <View style={{ marginVertical: 10 }}>
       <TextInput
         placeholder={`Athlete ${item.key}`}
         placeholderTextColor="gray"
         style={{ borderWidth: 1, padding: 10, fontSize: 16, color: 'black' }}
+        onChangeText={(text) => {
+          const updatedValues = [...inputValues];
+          updatedValues[index] = text;
+          setInputValues(updatedValues);
+        }}
+        value={inputValues[index]} 
       />
     </View>
   );
 
+  const handleEnterAthletes = () => {
+    navigation.navigate('TimerScreen', { inputValues });
+  };
+
   return (
     <View>
       <FlatList
-        data={data}
+        data={Array.from({ length: userNumber }, (_, index) => ({ key: String(index+1) }))}
         renderItem={renderItem}
         keyExtractor={(item) => item.key}
       />
-      <Button title="Enter Athletes" onPress={() => navigation.navigate('TimerScreen')} />
+      <Button title="Enter Athletes" onPress={handleEnterAthletes} />
     </View>
   );
 };
--- Components/TimerScreen.jsx
+++ Components/TimerScreen.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 
 const TimerScreen = () => {
+  const route = useRoute();
+  const inputValues = route.params?.inputValues || '';
   return (
     <View>
       <Text>This is the TimerScreen</Text>
+      {inputValues.map((value, index) => (
+        <Text key={index}>{`Athlete ${index + 1}: ${value}`}</Text>
+      ))}
     </View>
   );
 };
